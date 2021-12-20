1837f5310a40f4a913a29c0e75d7a1d1c168f4ce
Mike Candy <mrcandy@me.com>
2021-12-20 15:24:07 -0700

    Added guizero and tested

--- main.py
+++ main.py
@@ -4,13 +4,10 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    from guizero import App
+    app = App(title="hello World")
+    app.display()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
