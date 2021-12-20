497584965412025e861cb25dd0829793e8339e41
mike@pi <mrcandy@me.com>
2021-12-20 16:41:16 -0700

    added test text

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Python script to control fuel transfer and cleaning between tanks
 
 if __name__ == '__main__':
-    from guizero import App
+    from guizero import App, Text
 
     app = App(title='Fuel Tank Control', width=1024, height=600)
+
+    t = Text(app, text="Test")
 
     app.display()
