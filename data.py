a77b37dbdef2d08151027e63cb609ec29d939c7c
Mike Candy <mrcandy@me.com>
2021-12-22 12:11:44 -0700

    Fixed background sizes/colors

--- data.py
+++ data.py
@@ -6,48 +6,56 @@ class CreateData:
         self.app = app
         self.pid = pid
         self.box = Box(self.app, layout="grid")
+        self.box.bg = "grey20"
+        self.box.width = "fill"
         self.portSrc = self.portDst = self.stbdSrc = self.stbdDst = None
         self.volume = 0
         self.time = 0
 
-        Text(self.box, text="Port", size=24, color="red", grid=[0, 0])
-        Text(self.box, text="xx.x", size=24, color="white", grid=[0, 1])
-        Text(self.box, text="xxx", size=24, color="white", grid=[0, 2])
+        Text(self.box, text="", color="white", grid=[0, 0], width=15)
 
-        Text(self.box, text="%", size=24, color="white", grid=[1, 1])
-        Text(self.box, text="L", size=24, color="white", grid=[1, 2])
+        Text(self.box, text="Port", size=24, color="red", grid=[1, 0])
+        Text(self.box, text="xx.x", size=24, color="white", grid=[1, 1])
+        Text(self.box, text="xxx", size=24, color="white", grid=[1, 2])
 
-        Text(self.box, text="", color="white", grid=[2, 0], width=15)
+        Text(self.box, text="%", size=24, color="white", grid=[2, 1])
+        Text(self.box, text="L", size=24, color="white", grid=[2, 2])
 
-        Text(self.box, text="Transfer:", size=24, color="white", grid=[3, 0], align="right")
-        Text(self.box, text="Time:", size=24, color="white", grid=[3, 1], align="right")
-        Text(self.box, text="Source:", size=24, color="white", grid=[3, 2], align="right")
-        Text(self.box, text="Destination:", size=24, color="white", grid=[3, 3], align="right")
+        Text(self.box, text="", color="white", grid=[3, 0], width=15)
 
-        TextBox(self.box, grid=[4, 0])
-        TextBox(self.box, grid=[4, 1])
-        self.portSrc = CheckBox(self.box, text="Port", grid=[4, 2])
-        self.portDst = CheckBox(self.box, text="Port", grid=[4, 3])
+        Text(self.box, text="Transfer:", size=24, color="white", grid=[4, 0], align="right")
+        Text(self.box, text="Time:", size=24, color="white", grid=[4, 1], align="right")
+        Text(self.box, text="Source:", size=24, color="white", grid=[4, 2], align="right")
+        Text(self.box, text="Destination:", size=24, color="white", grid=[4, 3], align="right")
 
-        Text(self.box, text="L", size=24, color="white", grid=[5, 0], align="left")
-        Text(self.box, text="min:sec", size=24, color="white", grid=[5, 1], align="left")
-        self.stbdSrc = CheckBox(self.box, text="Stbd", grid=[5, 2])
-        self.stbdDst = CheckBox(self.box, text="Stbd", grid=[5, 3])
+        TextBox(self.box, grid=[5, 0])
+        TextBox(self.box, grid=[5, 1])
+        self.portSrc = CheckBox(self.box, text="Port", grid=[5, 2])
+        self.portDst = CheckBox(self.box, text="Port", grid=[5, 3])
+        self.portSrc.bg = self.portDst.bg = "red"
 
-        self.volUp = PushButton(self.box, text="^", grid=[6, 0], image="up_arrow.png", pady=5)
-        self.timUp = PushButton(self.box, text="^", grid=[6, 1], image="up_arrow.png", pady=5)
+        Text(self.box, text="L", size=24, color="white", grid=[6, 0], align="left")
+        Text(self.box, text="min:sec", size=24, color="white", grid=[6, 1], align="left")
+        self.stbdSrc = CheckBox(self.box, text="Stbd", grid=[6, 2])
+        self.stbdDst = CheckBox(self.box, text="Stbd", grid=[6, 3])
+        self.stbdSrc.bg = self.stbdDst.bg = "green"
 
-        self.volDn = PushButton(self.box, text="v", grid=[7, 0], image="down_arrow.png", pady=5)
-        self.timDn = PushButton(self.box, text="v", grid=[7, 1], image="down_arrow.png", pady=5)
+        self.volUp = PushButton(self.box, text="^", grid=[7, 0], image="up_arrow.png", pady=5)
+        self.timUp = PushButton(self.box, text="^", grid=[7, 1], image="up_arrow.png", pady=5)
+        self.volUp.bg = self.timUp.bg = "white"
 
-        Text(self.box, text="", color="white", grid=[8, 0], width=15)
+        self.volDn = PushButton(self.box, text="v", grid=[8, 0], image="down_arrow.png", pady=5)
+        self.timDn = PushButton(self.box, text="v", grid=[8, 1], image="down_arrow.png", pady=5)
+        self.volDn.bg = self.timDn.bg = "white"
 
-        Text(self.box, text="Stbd", size=24, align="top", color="green", grid=[9, 0])
-        Text(self.box, text="xx.x", size=24, color="white", grid=[9, 1])
-        Text(self.box, text="xxx", size=24, color="white", grid=[9, 2])
+        Text(self.box, text="", color="white", grid=[9, 0], width=18)
 
-        Text(self.box, text="%", size=24, color="white", grid=[10, 1])
-        Text(self.box, text="L", size=24, color="white", grid=[10, 2])
+        Text(self.box, text="Stbd", size=24, align="top", color="green", grid=[10, 0])
+        Text(self.box, text="xx.x", size=24, color="white", grid=[10, 1])
+        Text(self.box, text="xxx", size=24, color="white", grid=[10, 2])
+
+        Text(self.box, text="%", size=24, color="white", grid=[11, 1])
+        Text(self.box, text="L", size=24, color="white", grid=[11, 2])
 
         self.portSrc.update_command(self.doValve, [self.portSrc])
         self.portDst.update_command(self.doValve, [self.portDst])
